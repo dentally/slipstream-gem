787e9c7fb16d1d15cb42084551c5747b75aaaf16
Jonny Wheeler <jonny@yakshak.co.uk>
2024-08-09 15:24:07 +0100

    Add build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Continue with other build steps
+echo "Generating client..."
+openapi-generator generate -i https://dev.slipstream.hsone.app/swagger/v1/swagger.json -g ruby -c openapi-generator-gem-config.yaml -o . --git-repo-id slipstream-gem --git-user-id dentally
+
+# Apply patch config patch to remove dup hash key
+echo "Applying patch..."
+
+git apply dedup_configuration_auth_settings.patch
